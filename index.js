4b6e29a11531ecbf69851dbc4fcf601c91ccb83c
Aqua200 <chinquepapa@gmail.com>
2024-12-05 15:23:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Goku-\nBlack\nBot-MD', {
+say('Edeb', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • Ivan-Creador`, {
+say(`Developed By • Aqua200-Creador`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
